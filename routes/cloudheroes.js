858feb3c0725c64c319c105d9a664353805a5da3
JialiuWuxi <37652140+JialiuWuxi@users.noreply.github.com>
2018-12-06 21:58:18 +0800

    update cloudhero service

--- routes/cloudheroes.js
+++ routes/cloudheroes.js
@@ -6,5 +6,12 @@ router.get('/', (req, res) => {
     cloudHeroService.getHeroes(req, res);
 })
 
+router.post('/', (req, res, next) =>{
+    cloudHeroService.dataVerify(req, res);
+    next();
+},(req, res) => {
+    cloudHeroService.postHeroes(req, res);
+})
+
 
 module.exports = router;
--- service/cloudheroes.service.js
+++ service/cloudheroes.service.js
@@ -15,6 +15,39 @@ function getHeroes(req, res) {
         }); 
 }
 
+function postHeroes(req, res) {
+    const newHero = {
+        name: req.body.name,
+        MPNid: req.body.MPNid,
+        benefitLevel: req.body.benefitLevel,
+        region: req.body.region,
+        ptc: req.body.ptc,
+    };
+
+    cloudHeroes = new CloudHero(newHero);
+
+    cloudHeroes.save(error =>{
+        if(checkServerError(res, error)) return;
+        res.status(201).send(cloudHeroes);
+        console.log('cloud hero created successfully!')
+    })
+}
+
+function dataVerify(req, res) {
+    if(!req.body.MPNid || !req.body.ptc || !req.body.region || !req.body.benifitLevel){
+        res.status(400).send('missing some data' + JSON.stringify(req.body));
+    }
+}
+
+function checkServerError(res, error) {
+    if(error){
+        res.status(500).send(error);
+        return error;
+    }
+}
+
 module.exports = {
     getHeroes,
+    postHeroes,
+    dataVerify,
 }
