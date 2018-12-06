649a4f838406399aa27ed85a9add6646a51adf27
JialiuWuxi <37652140+JialiuWuxi@users.noreply.github.com>
2018-12-06 21:24:19 +0800

    Update mongodb.js

--- mongodb/mongodb.js
+++ mongodb/mongodb.js
@@ -2,16 +2,13 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-// const mongoUrl = `mongodb://${process.env.dbName}.documents.azure.com:${process.env.cosmosPort}/${process.env.dbName}?ssl=true`;
-const mongoUrl = 'mongodb://cloudhero.documents.azure.com:10255/cloudhero?ssl=true';
+const mongoUrl = `mongodb://${process.env.dbName}.documents.azure.com:${process.env.cosmosPort}/${process.env.dbName}?ssl=true`;
 
 function connect(){
     return mongoose.connect(mongoUrl, {
         auth: {
-            // user: `${process.env.dbName}`,
-            // password: `${process.env.key}`
-            user: 'cloudhero',
-            password: 'QojcqqTqgXnbZXM7JsQ215wSB8iAF8CBgNsCH85vGgRoKTh5jzVJaaJi5tstKL3At4YSrgBq9L43TedYYhsBjw=='
+            user: `${process.env.dbName}`,
+            password: `${process.env.key}`
         },
         useNewUrlParser: true,
         useCreateIndex: false,
